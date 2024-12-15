98452999e94ad22a365e599ab021fa0462256a25
Edward Banner <edward.banner@gmail.com>
2024-12-15 08:26:46 -0500

    ★ Day 15

--- day15/a.py
+++ day15/a.py
@@ -15,11 +15,7 @@ def get_map(input_str):
 
 def get_instructions(input_str):
     _, instructions_str = input_str.split('\n\n')
-
     instructions = ''.join(instructions_str.strip())
-
-    print('instructions', instructions)
-
     return instructions
 
 
@@ -28,10 +24,6 @@ def get_position(map):
         for j in range(len(map[0])):
             if map[i][j] == '@':
                 return i, j
-
-
-def get_gps_coordinates(map):
-    return [0]
 
 
 def try_move(i, j, instruction, map):
@@ -84,7 +76,7 @@ def try_move(i, j, instruction, map):
                 return map
 
             if map[i][k] == '.':
-                map[i][k:j-1] = 'O'
+                map[i][k:j-1] = ['O']*(j-k-1)
                 map[i][j-1], map[i][j] = '@.'
                 return map
 
@@ -92,7 +84,23 @@ def try_move(i, j, instruction, map):
             k -= 1
 
     elif instruction == '^':
-        raise NotImplementedError('^', 'try_move')
+        k = i-2
+        while True:
+            if k < 0:
+                return map
+
+            if map[k][j] == '#':
+                return map
+
+            if map[k][j] == '.':
+                for l in range(k, i-1):
+                    map[l][j] = 'O'
+                map[i-1][j] = '@'
+                map[i][j] = '.'
+                return map
+
+            assert map[k][j] == 'O'
+            k -= 1
 
 
 def execute(instruction, map):
@@ -107,22 +115,22 @@ def execute(instruction, map):
             map = try_move(i, j, '<', map)
     elif instruction == '^':
         if map[i-1][j] == '#':
-            raise NotImplementedError('^', '#')
+            pass
         elif map[i-1][j] == '.':
             map[i-1][j] = '@'
             map[i][j] = '.'
         elif map[i-1][j] == 'O':
-            raise NotImplementedError('^', 'O')
+            map = try_move(i, j, '^', map)
     elif instruction == '>':
         if map[i][j+1] == '#':
-            raise NotImplementedError('>', '#')
+            pass
         elif map[i][j+1] == '.':
-            raise NotImplementedError('>', '.')
+            map[i][j], map[i][j+1] = '.@'
         elif map[i][j+1] == 'O':
             map = try_move(i, j, '>', map)
     elif instruction == 'v':
         if map[i+1][j] == '#':
-            raise NotImplementedError('v', '#')
+            pass
         elif map[i+1][j] == '.':
             map[i][j] = '.'
             map[i+1][j] = '@'
@@ -132,23 +140,21 @@ def execute(instruction, map):
     return map
 
 
+def get_gps_coordinates_sum(map):
+    gps_coordinates_sum = 0
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 'O':
+                gps_coordinates_sum += 100*i + j
+    return gps_coordinates_sum
+
+
 if __name__ == '__main__':
     input_str = get_input()
     map, instructions = get_map(input_str), get_instructions(input_str)
 
-    print(instructions)
-
-    for row in map:
-        print(''.join(row))
-    print()
-
     for instruction in instructions:
         map = execute(instruction, map)
 
-        print(f'Move {instruction}:')
-        for row in map:
-            print(''.join(row))
-        print()
-
-    gps_coordinates = get_gps_coordinates(map)
-    print(sum(gps_coordinates))
+    gps_coordinates_sum = get_gps_coordinates_sum(map)
+    print(gps_coordinates_sum)
